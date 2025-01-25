d1e1b66f93ac8937c3e6772475ed11ff858ad44f
WailBenoulha <wailbnlh@gmail.com>
2025-01-25 06:39:28 -1100

    fix

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -5,7 +5,7 @@ set -e
 # TODO: Set to URL of git repo.
 PROJECT_GIT_URL='https://github.com/WailBenoulha/Markdown-Note-taking-app.git'
 
-PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
+PROJECT_BASE_PATH='/usr/local/apps/markdown'
 
 echo "Installing dependencies..."
 apt-get update
